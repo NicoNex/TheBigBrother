ea53b62dad2e02a8d679f5a0d03d0b42a6612e1e
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2022-04-08 16:58:00 +0200

    Add installer.

---
+++ installer.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function install {
+	chmod +x bigbrother
+	cp bigbrother $HOME/.local/bin
+	mkdir -p $HOME/.local/share/bigbrother
+	cp police.mp3 $HOME/.local/share/bigbrother
+	echo ok
+}
+
+function uninstall {
+	rm -rf $HOME/.local/bin/bigbrother $HOME/.local/share/bigbrother
+	echo ok
+}
+
+function usage {
+	cat << EOF
+Usage: $0 OPTIONS
+This script installs or uninstalls The Big Brother.
+OPTIONS:
+	-i            Install The Big Brother.
+	-r            Uninstall The Big Brother.
+	-h, --help    Print this help message.
+EOF
+}
+
+case $1 in
+	-h | --help)
+		usage
+		;;
+	-i)
+		install
+		;;
+	-r)
+		uninstall
+		;;
+	*)
+		usage
+		;;
+esac
+
